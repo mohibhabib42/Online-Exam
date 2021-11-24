72f48e22cc260f678a6a5853776ef2ca6e18055a
Mohib Habib <60970690+mohibhabib42@users.noreply.github.com>
2021-11-24 13:31:19 +0500

    Update dbconfiguration.js

--- src/dbconfiguration.js
+++ src/dbconfiguration.js
@@ -3,12 +3,12 @@ import "firebase/auth";
 import "firebase/firestore"
 
 const app=firebase.initializeApp({
-    apiKey: "AIzaSyAjxtSBpKQS6lbOvUY5scNdR50904qtxMM",
-    authDomain: "onlineexam-586bf.firebaseapp.com",
-    projectId: "onlineexam-586bf",
-    storageBucket: "onlineexam-586bf.appspot.com",
-    messagingSenderId: "97266709550",
-    appId: "1:97266709550:web:f26aae7384c1469e7edbbf"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
   });
   export const auth =app.auth()
   export default app
